phytec/imx-sm-phytec

commit de05d4a91455f6785dd2deab58385a732f79eca7

Adapt copied code to Libra i.MX 95
Mostly remove code for hardware that is not present on Libra i.MX 95.
- GPIO expander
- RTC

adapt config as Libra also has modules/resources used differently. E.g.
debug UART, CAN.

Signed-off-by: Yannic Moog <y.moog@phytec.de>
Signed-off-by: Benjamin Hahn <B.Hahn@phytec.de>

boards/mcimx95libra/board.c
@@ -1,5 +1,6 @@
 /*
  * Copyright 2023-2024 NXP
+ * Copyright 2025 PHYTEC Messtechnik GmbH
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -25,24 +26,24 @@
  ******************************************************************************/
 
 /* External board-level clock rates */
-#define BOARD_EXT_CLK_RATE          25000000UL  /* 25MHz */
+#define BOARD_EXT_CLK_RATE      25000000UL  /* 25MHz */
 
 /* ADC clck rate */
-#define BOARD_ADC_CLK_RATE          80000000UL  /* 80MHz */
+#define BOARD_ADC_CLK_RATE      80000000UL  /* 80MHz */
 
 /* SM SysTick parameters */
 #define BOARD_SYSTICK_CLKSRC    0U                      /* 0 = external ref  */
 #define BOARD_SYSTICK_CLK_ROOT  CLOCK_ROOT_M33SYSTICK   /* Dedicated CCM root */
 
 /* SM WDOG */
-#define BOARD_WDOG_BASE_PTR         WDOG2
-#define BOARD_WDOG_IRQn             WDOG2_IRQn
-#define BOARD_WDOG_CLK_SRC          kWDOG32_ClockSource1  /* lpo_clk @ 32K */
-#define BOARD_WDOG_TIMEOUT          0xFFFFU  /* 65535 ticks @ 32K = 2 sec */
-#define BOARD_WDOG_SRMASK           (1UL << RST_REASON_WDOG2)
-#define BOARD_WDOG_ANY_INIT         ~(BLK_CTRL_S_AONMIX_WDOG_ANY_MASK_WDOG2_MASK)
-#define BOARD_WDOG_ANY_MASK         BLK_CTRL_S_AONMIX_WDOG_ANY_MASK_WDOG2_MASK
-#define BOARD_WDOG_IPG_DEBUG        BLK_CTRL_NS_AONMIX_IPG_DEBUG_CM33_WDOG2_MASK
+#define BOARD_WDOG_BASE_PTR     WDOG2
+#define BOARD_WDOG_IRQn         WDOG2_IRQn
+#define BOARD_WDOG_CLK_SRC      kWDOG32_ClockSource1  /* lpo_clk @ 32K */
+#define BOARD_WDOG_TIMEOUT      0xFFFFU  /* 65535 ticks @ 32K = 2 sec */
+#define BOARD_WDOG_SRMASK       (1UL << RST_REASON_WDOG2)
+#define BOARD_WDOG_ANY_INIT     ~(BLK_CTRL_S_AONMIX_WDOG_ANY_MASK_WDOG2_MASK)
+#define BOARD_WDOG_ANY_MASK     BLK_CTRL_S_AONMIX_WDOG_ANY_MASK_WDOG2_MASK
+#define BOARD_WDOG_IPG_DEBUG    BLK_CTRL_NS_AONMIX_IPG_DEBUG_CM33_WDOG2_MASK
 
 /* Board UART */
 #ifdef INC_LIBC
@@ -51,10 +52,18 @@
 #define BOARD_UART                  0U
 #endif
 
+#define DDR_NONCACHEABLE  (1U)
+#define DDR_WRITE_THROUGH (0U)
+#define DDR_WRITE_BACK    (0U)
+
+#define OCRAM_NONCACHEABLE  (1U)
+#define OCRAM_WRITE_THROUGH (0U)
+#define OCRAM_WRITE_BACK    (0U)
+
 /*******************************************************************************
  * Variables
  ******************************************************************************/
-static wdog32_config_t s_wdogConfig;
+static wdog32_config_t s_wdogConfig = { };
 
 /* Debug UART base pointer list */
 static LPUART_Type *const s_uartBases[] = LPUART_BASE_PTRS;
@@ -63,8 +72,7 @@ static LPUART_Type *const s_uartBases[] = LPUART_BASE_PTRS;
 static IRQn_Type const s_uartIrqs[] = LPUART_RX_TX_IRQS;
 
 /* Debug UART clock list */
-static uint32_t const s_uartClks[] =
-{
+static uint32_t const s_uartClks[] = {
     0U,
     CLOCK_ROOT_LPUART1,
     CLOCK_ROOT_LPUART2,
@@ -77,8 +85,7 @@ static uint32_t const s_uartClks[] =
 };
 
 /* Debug UART peripheral LPI list */
-static uint32_t const s_uartPerLpi[] =
-{
+static uint32_t const s_uartPerLpi[] = {
     0U,
     CPU_PER_LPI_IDX_LPUART1,
     CPU_PER_LPI_IDX_LPUART2,
@@ -91,8 +98,7 @@ static uint32_t const s_uartPerLpi[] =
 };
 
 /* Debug UART configuration info */
-static board_uart_config_t const s_uartConfig =
-{
+static board_uart_config_t const s_uartConfig = {
     .base = s_uartBases[BOARD_UART],
     .irq = s_uartIrqs[BOARD_UART],
     .clockId = s_uartClks[BOARD_UART],
@@ -108,9 +114,7 @@ static board_uart_config_t const s_uartConfig =
 /*--------------------------------------------------------------------------*/
 /* Configure CM33 MPU and XCACHE controller                                 */
 /*--------------------------------------------------------------------------*/
-void BOARD_ConfigMPU(void)
-{
-    uint8_t attr;
+void BOARD_ConfigMPU(void) {
 
     /* Disable code cache(ICache) and system cache(DCache) */
     XCACHE_DisableCache(LPCAC_PC);
@@ -152,7 +156,7 @@ void BOARD_ConfigMPU(void)
     /* Attr2: Normal memory, Inner write-through transient, read allocate.
      * Inner write-through transient, read allocate
      */
-    attr = ARM_MPU_ATTR_MEMORY_(0U, 0U, 1U, 0U);
+    uint8_t attr = ARM_MPU_ATTR_MEMORY_(0U, 0U, 1U, 0U);
     ARM_MPU_SetMemAttr(2U, ARM_MPU_ATTR(attr, attr));
 
     /* Attr3: Normal memory, Outer write-back transient, read/write allocate.
@@ -182,9 +186,6 @@ void BOARD_ConfigMPU(void)
      * #define DDR_WRITE_THROUGH (0U)
      * #define DDR_WRITE_BACK (1U)
      */
-#define DDR_NONCACHEABLE  (1U)
-#define DDR_WRITE_THROUGH (0U)
-#define DDR_WRITE_BACK    (0U)
 #if DDR_NONCACHEABLE
     /* NOTE: DDR is used as shared memory for A/M core communication,
      * set it to non-cacheable. */
@@ -208,9 +209,6 @@ void BOARD_ConfigMPU(void)
         1U, 0U), ARM_MPU_RLAR(0xDFFFFFFFU, 3U));
 #endif
 
-#define OCRAM_NONCACHEABLE  (1U)
-#define OCRAM_WRITE_THROUGH (0U)
-#define OCRAM_WRITE_BACK    (0U)
 #if OCRAM_NONCACHEABLE
     /* Region 1: [0x20480000, 0x2051FFFF](OCRAM), outer shareable, read/write,
      * any privileged, executable. Attr 1 (non-cacheable). */
@@ -244,25 +242,16 @@ void BOARD_ConfigMPU(void)
 /*--------------------------------------------------------------------------*/
 /* Initialize clocking                                                      */
 /*--------------------------------------------------------------------------*/
-void BOARD_InitClocks(void)
-{
+void BOARD_InitClocks(void) {
     uint32_t fuseTrim = FSB->FUSE[FSB_FUSE_ANA_CFG4];
 
-    if (fuseTrim == 0U)
-    {
+    if (!fuseTrim) {
         /* Enable the FRO clock with default value */
         (void)FRO_SetEnable(true);
-    }
-    else
-    {
-        /* Set the Trim value read from the fuses */
-        bool status = FRO_SetTrim(fuseTrim);
-
-        if (status)
-        {
+    } else {
+        if (FRO_SetTrim(fuseTrim))
             /* Enable the FRO clock with default value */
             (void) FRO_SetEnable(true);
-        }
     }
 
     /* Configure default EXT_CLK1 rate tied to XTAL_OUT/EXT_CLK pin */
@@ -277,22 +266,19 @@ void BOARD_InitClocks(void)
 /*--------------------------------------------------------------------------*/
 /* Return the debug UART info                                               */
 /*--------------------------------------------------------------------------*/
-const board_uart_config_t *BOARD_GetDebugUart(void)
-{
+const board_uart_config_t *BOARD_GetDebugUart(void) {
     return &s_uartConfig;
 }
 
 /*--------------------------------------------------------------------------*/
 /* Initialize debug console                                                 */
 /*--------------------------------------------------------------------------*/
-void BOARD_InitDebugConsole(void)
-{
-    if (s_uartConfig.base != NULL)
-    {
+void BOARD_InitDebugConsole(void) {
+    if (s_uartConfig.base) {
         uint64_t rate = CCM_RootGetRate(s_uartConfig.clockId);
 
         /* Configure debug UART */
-        lpuart_config_t lpuart_config;
+        lpuart_config_t lpuart_config = { };
         LPUART_GetDefaultConfig(&lpuart_config);
         lpuart_config.baudRate_Bps = s_uartConfig.baud;
         lpuart_config.rxFifoWatermark = ((uint8_t)
@@ -301,29 +287,22 @@ void BOARD_InitDebugConsole(void)
             FSL_FEATURE_LPUART_FIFO_SIZEn(s_uartConfig.base)) - 1U;
         lpuart_config.enableTx = true;
         lpuart_config.enableRx = true;
-        (void) LPUART_Init(s_uartConfig.base, &lpuart_config,
-            (uint32_t) rate & 0xFFFFFFFFU);
+        (void) LPUART_Init(s_uartConfig.base, &lpuart_config, (uint32_t) rate & 0xFFFFFFFFU);
     }
 }
 
 /*--------------------------------------------------------------------------*/
 /* Initialize IRQ handlers                                                  */
 /*--------------------------------------------------------------------------*/
-void BOARD_InitHandlers(void)
-{
+void BOARD_InitHandlers(void) {
     /* Configure default priority of exceptions and IRQs */
-    for (int32_t irq = ((int32_t) SVCall_IRQn); irq < ((int32_t)
-        NUMBER_OF_INT_VECTORS); irq++)
-    {
+    for (int32_t irq = SVCall_IRQn; irq < NUMBER_OF_INT_VECTORS; irq++) {
         // coverity[misra_c_2012_rule_10_5_violation:FALSE]
         NVIC_SetPriority((IRQn_Type) irq, IRQ_PRIO_NOPREEMPT_NORMAL);
     }
 
     /* Configure SWI handler */
     NVIC_EnableIRQ(BOARD_SWI_IRQn);
-
-    /* Enable BBNSM handler */
-    NVIC_EnableIRQ(BBNSM_IRQn);
 
     /* Enable GPC SM handler */
     NVIC_SetPriority(GPC_SM_REQ_IRQn, IRQ_PRIO_NOPREEMPT_VERY_HIGH);
@@ -337,18 +316,14 @@ void BOARD_InitHandlers(void)
     /* Enable FCCU handler */
     NVIC_SetPriority(FCCU_INT0_IRQn, IRQ_PRIO_NOPREEMPT_CRITICAL);
     NVIC_EnableIRQ(FCCU_INT0_IRQn);
-
-    /* Enable GPIO1 handler */
-    NVIC_EnableIRQ(GPIO1_0_IRQn);
 }
 
 /*--------------------------------------------------------------------------*/
 /* Initialize timers                                                        */
 /*--------------------------------------------------------------------------*/
-void BOARD_InitTimers(void)
-{
+void BOARD_InitTimers(void) {
     /* Configure and enable the BBNSM RTC */
-    bbnsm_rtc_config_t rtcConfig;
+    bbnsm_rtc_config_t rtcConfig = { };
     BBNSM_RTC_GetDefaultConfig(&rtcConfig);
     BBNSM_RTC_Init(BBNSM, &rtcConfig);
 
@@ -388,10 +363,8 @@ void BOARD_InitTimers(void)
 /*--------------------------------------------------------------------------*/
 /* Set watchdog mode                                                        */
 /*--------------------------------------------------------------------------*/
-void BOARD_WdogModeSet(uint32_t mode)
-{
-    switch (mode)
-    {
+void BOARD_WdogModeSet(uint32_t mode) {
+    switch (mode) {
         case BOARD_WDOG_MODE_WARM: /* warm */
             /* Allow WDOG to generate internal warm reset */
             SRC_GEN->SRMASK &= (~BOARD_WDOG_SRMASK);
@@ -457,27 +430,25 @@ void BOARD_WdogModeSet(uint32_t mode)
 /*--------------------------------------------------------------------------*/
 /* Kick the watchdog timer                                                  */
 /*--------------------------------------------------------------------------*/
-void BOARD_WdogRefresh(void)
-{
+void BOARD_WdogRefresh(void) {
     WDOG32_Refresh(BOARD_WDOG_BASE_PTR);
 }
 
 /*--------------------------------------------------------------------------*/
 /* Initialize serial bus for external devices                               */
 /*--------------------------------------------------------------------------*/
-void BOARD_InitSerialBus(void)
-{
+void BOARD_InitSerialBus(void) {
     static LPI2C_Type *const s_i2cBases[] = LPI2C_BASE_PTRS;
     LPI2C_Type *base = s_i2cBases[BOARD_I2C_INSTANCE];
-    lpi2c_master_config_t lpi2cConfig = {0};
-    static uint32_t const s_i2cClks[] =
-    {
+    lpi2c_master_config_t lpi2cConfig = { };
+    static uint32_t const s_i2cClks[] = {
         0U,
         CLOCK_ROOT_LPI2C1,
         CLOCK_ROOT_LPI2C2
     };
     uint32_t clockId = s_i2cClks[BOARD_I2C_INSTANCE];
 
+    /* bug, reduce width from 64 to 32 */
     uint32_t rate = (uint32_t) CCM_RootGetRate(clockId);
 
     LPI2C_MasterGetDefaultConfig(&lpi2cConfig);
@@ -491,35 +462,26 @@ void BOARD_InitSerialBus(void)
 /*--------------------------------------------------------------------------*/
 /* System sleep prepare                                                     */
 /*--------------------------------------------------------------------------*/
-void BOARD_SystemSleepPrepare(uint32_t sleepMode, uint32_t sleepFlags)
-{
+void BOARD_SystemSleepPrepare(uint32_t sleepMode, uint32_t sleepFlags) {
     BRD_SM_VoltageSuspend(true);
 
     /* Configure SM LPUART for wakeup */
-    if (s_uartConfig.base != NULL)
-    {
+    if (s_uartConfig.base) {
         /* Enable edge-detect IRQ */
-        (void) LPUART_ClearStatusFlags(s_uartConfig.base,
-            (uint32_t)kLPUART_RxActiveEdgeFlag);
-        LPUART_EnableInterrupts(s_uartConfig.base,
-            (uint32_t)kLPUART_RxActiveEdgeInterruptEnable);
+        (void) LPUART_ClearStatusFlags(s_uartConfig.base, (uint32_t)kLPUART_RxActiveEdgeFlag);
+        LPUART_EnableInterrupts(s_uartConfig.base, (uint32_t)kLPUART_RxActiveEdgeInterruptEnable);
         NVIC_EnableIRQ(s_uartConfig.irq);
 
         /* Configure LPI of SM LPUART */
         (void) CPU_PerLpiConfigSet(CPU_IDX_M33P, s_uartConfig.perLpiId,
-            CPU_PER_LPI_ON_RUN_WAIT_STOP);
+                                   CPU_PER_LPI_ON_RUN_WAIT_STOP);
     }
-
-    /* Configure LPI for GPIO1 */
-    (void) CPU_PerLpiConfigSet(CPU_IDX_M33P, CPU_PER_LPI_IDX_GPIO1,
-        CPU_PER_LPI_ON_RUN_WAIT_STOP);
 }
 
 /*--------------------------------------------------------------------------*/
 /* System sleep entry                                                       */
 /*--------------------------------------------------------------------------*/
-void BOARD_SystemSleepEnter(uint32_t sleepMode, uint32_t sleepFlags)
-{
+void BOARD_SystemSleepEnter(uint32_t sleepMode, uint32_t sleepFlags) {
     /* Disable SysTick */
     uint32_t sysTickMask = SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk;
     SysTick->CTRL &= (~sysTickMask);
@@ -527,14 +489,12 @@ void BOARD_SystemSleepEnter(uint32_t sleepMode, uint32_t sleepFlags)
     /* Clear pending SysTick exception */
     SCB->ICSR = SCB_ICSR_PENDSTCLR_Msk;
 
-    if (s_wdogConfig.enableWdog32)
-    {
+    if (s_wdogConfig.enableWdog32) {
         /* Disable WDOG */
         WDOG32_Deinit(BOARD_WDOG_BASE_PTR);
 
         /* Waits for new configuration to take effect. */
-        while (0U == ((BOARD_WDOG_BASE_PTR->CS) & WDOG_CS_RCS_MASK))
-        {
+        while (0U == ((BOARD_WDOG_BASE_PTR->CS) & WDOG_CS_RCS_MASK)) {
             ; /* Intentional empty while */
         }
     }
@@ -543,13 +503,10 @@ void BOARD_SystemSleepEnter(uint32_t sleepMode, uint32_t sleepFlags)
 /*--------------------------------------------------------------------------*/
 /* System sleep exit                                                        */
 /*--------------------------------------------------------------------------*/
-void BOARD_SystemSleepExit(uint32_t sleepMode, uint32_t sleepFlags)
-{
+void BOARD_SystemSleepExit(uint32_t sleepMode, uint32_t sleepFlags) {
     if (s_wdogConfig.enableWdog32)
-    {
         /* Enable WDOG */
         WDOG32_Init(BOARD_WDOG_BASE_PTR, &s_wdogConfig);
-    }
 
     /* Enable SysTick */
     uint32_t sysTickMask = SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk;
@@ -560,20 +517,15 @@ void BOARD_SystemSleepExit(uint32_t sleepMode, uint32_t sleepFlags)
 /*--------------------------------------------------------------------------*/
 /* System sleep unprepare                                                   */
 /*--------------------------------------------------------------------------*/
-void BOARD_SystemSleepUnprepare(uint32_t sleepMode, uint32_t sleepFlags)
-{
+void BOARD_SystemSleepUnprepare(uint32_t sleepMode, uint32_t sleepFlags) {
     BRD_SM_VoltageRestore();
 
     /* Service SM LPUART wakeup events */
-    if (s_uartConfig.base != NULL)
-    {
-        (void) LPUART_ClearStatusFlags(s_uartConfig.base,
-            (uint32_t)kLPUART_RxActiveEdgeFlag);
-        LPUART_DisableInterrupts(s_uartConfig.base,
-            (uint32_t)kLPUART_RxActiveEdgeInterruptEnable);
+    if (s_uartConfig.base) {
+        (void) LPUART_ClearStatusFlags(s_uartConfig.base, (uint32_t)kLPUART_RxActiveEdgeFlag);
+        LPUART_DisableInterrupts(s_uartConfig.base, (uint32_t)kLPUART_RxActiveEdgeInterruptEnable);
 
         NVIC_DisableIRQ(s_uartConfig.irq);
         NVIC_ClearPendingIRQ(s_uartConfig.irq);
     }
 }
-

boards/mcimx95libra/board.h
@@ -1,5 +1,6 @@
 /*
  * Copyright 2023-2024 NXP
+ * Copyright 2025 PHYTEC Messtechnik GmbH
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -63,8 +64,7 @@
 /*!
  * Debug UART configuration info
  */
-typedef struct
-{
+typedef struct {
     LPUART_Type *const base;  /*!< LPUART base pointer */
     IRQn_Type irq;            /*!< Interrupt number */
     uint32_t clockId;         /*!< Clock ID */
@@ -81,13 +81,8 @@ extern "C" {
  * API
  ******************************************************************************/
 
-/*! Configure the M33 MPU */
 void BOARD_ConfigMPU(void);
-
-/*! Init hardware */
 void BOARD_InitHardware(void);
-
-/*! Init clocks */
 void BOARD_InitClocks(void);
 
 /*!
@@ -100,16 +95,9 @@ void BOARD_InitClocks(void);
  */
 const board_uart_config_t *BOARD_GetDebugUart(void);
 
-/*! Init the debug UART */
 void BOARD_InitDebugConsole(void);
-
-/*! Init interrupt handlers */
 void BOARD_InitHandlers(void);
-
-/*! Init timers */
 void BOARD_InitTimers(void);
-
-/*! Init serial buses */
 void BOARD_InitSerialBus(void);
 
 /*!
@@ -161,4 +149,3 @@ void BOARD_WdogRefresh(void);
 /** @} */
 
 #endif /* BOARD_H */
-

boards/mcimx95libra/pin_mux.c
@@ -1,5 +1,6 @@
 /*
  * Copyright 2023 NXP
+ * Copyright 2025 PHYTEC Messtechnik GmbH
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -13,34 +14,14 @@
  * Description   : Configures pin routing and optionally pin electrical features.
  *
  * END ****************************************************************************************************************/
-void BOARD_InitPins(void)
-{
-#if (BOARD_DEBUG_UART_INSTANCE == 1U)
-    /* Configure LPUART 1 */
-    IOMUXC_SetPinMux(IOMUXC_PAD_UART1_RXD__LPUART1_RX, 0U);
-    IOMUXC_SetPinConfig(IOMUXC_PAD_UART1_RXD__LPUART1_RX, IOMUXC_PAD_PD(1U));
-
-    IOMUXC_SetPinMux(IOMUXC_PAD_UART1_TXD__LPUART1_TX, 0);
-    IOMUXC_SetPinConfig(IOMUXC_PAD_UART1_TXD__LPUART1_TX, IOMUXC_PAD_DSE(0xFU));
-#elif (BOARD_DEBUG_UART_INSTANCE == 2U)
+void BOARD_InitPins(void) {
     /* Configure LPUART 2 */
     IOMUXC_SetPinMux(IOMUXC_PAD_UART2_RXD__LPUART2_RX, 0);
     IOMUXC_SetPinConfig(IOMUXC_PAD_UART2_RXD__LPUART2_RX, IOMUXC_PAD_PD(1U));
 
     IOMUXC_SetPinMux(IOMUXC_PAD_UART2_TXD__LPUART2_TX, 0);
     IOMUXC_SetPinConfig(IOMUXC_PAD_UART2_TXD__LPUART2_TX, IOMUXC_PAD_DSE(0xFU));
-#endif
 
-#if (BOARD_I2C_INSTANCE == 1U)
-    /* Configure LPI2C 1 */
-    IOMUXC_SetPinMux(IOMUXC_PAD_I2C1_SCL__LPI2C1_SCL, 1U);
-    IOMUXC_SetPinConfig(IOMUXC_PAD_I2C1_SCL__LPI2C1_SCL, IOMUXC_PAD_DSE(0xFU)
-        | IOMUXC_PAD_FSEL1(0x3U) | IOMUXC_PAD_PU(0x1U) | IOMUXC_PAD_OD(0x1U));
-
-    IOMUXC_SetPinMux(IOMUXC_PAD_I2C1_SDA__LPI2C1_SDA, 1U);
-    IOMUXC_SetPinConfig(IOMUXC_PAD_I2C1_SDA__LPI2C1_SDA, IOMUXC_PAD_DSE(0xFU)
-        | IOMUXC_PAD_FSEL1(0x3U) | IOMUXC_PAD_PU(0x1U) | IOMUXC_PAD_OD(0x1U));
-#elif (BOARD_I2C_INSTANCE == 2U)
     /* Configure LPI2C 2 */
     IOMUXC_SetPinMux(IOMUXC_PAD_I2C2_SCL__LPI2C2_SCL, 1U);
     IOMUXC_SetPinConfig(IOMUXC_PAD_I2C2_SCL__LPI2C2_SCL, IOMUXC_PAD_DSE(0xFU)
@@ -49,10 +30,4 @@ void BOARD_InitPins(void)
     IOMUXC_SetPinMux(IOMUXC_PAD_I2C2_SDA__LPI2C2_SDA, 1U);
     IOMUXC_SetPinConfig(IOMUXC_PAD_I2C2_SDA__LPI2C2_SDA, IOMUXC_PAD_DSE(0xFU)
         | IOMUXC_PAD_FSEL1(0x3U) | IOMUXC_PAD_PU(0x1U) | IOMUXC_PAD_OD(0x1U));
-#endif
-
-    /* Configure GPIO1-10 (INT from the PCAL6408A */
-    IOMUXC_SetPinMux(IOMUXC_PAD_PDM_BIT_STREAM1__GPIO1_IO_BIT10, 0U);
-    IOMUXC_SetPinConfig(IOMUXC_PAD_PDM_BIT_STREAM1__GPIO1_IO_BIT10, 0U);
 }
-

boards/mcimx95libra/sm/Makefile
@@ -40,31 +40,23 @@ INCLUDE +=  \
 	-I$(BOARDS_DIR)  \
 	-I$(BOARD_DIR)  \
 	-I$(BOARD_DIR)/sm \
-	-I$(COMPONENTS_DIR)/pcal6408a  \
 	-I$(COMPONENTS_DIR)/pf09  \
 	-I$(COMPONENTS_DIR)/pf53  \
-	-I$(COMPONENTS_DIR)/pca2131
 
 VPATH +=  \
 	$(BRD_SM_API_DIR)  \
 	$(BOARDS_DIR)  \
 	$(BOARD_DIR)  \
 	$(BOARD_DIR)/sm  \
-	$(COMPONENTS_DIR)/pcal6408a  \
 	$(COMPONENTS_DIR)/pf09  \
 	$(COMPONENTS_DIR)/pf53  \
-	$(COMPONENTS_DIR)/pca2131
 
 OBJS += \
 	$(OUT)/board.o  \
 	$(OUT)/brd_sm.o  \
 	$(OUT)/brd_sm_handlers.o  \
-	$(OUT)/brd_sm_control.o  \
 	$(OUT)/brd_sm_sensor.o  \
 	$(OUT)/brd_sm_voltage.o  \
-	$(OUT)/brd_sm_bbm.o  \
-	$(OUT)/fsl_pcal6408a.o  \
 	$(OUT)/fsl_pf09.o  \
 	$(OUT)/fsl_pf53.o  \
-	$(OUT)/fsl_pca2131.o
 

boards/mcimx95libra/sm/brd_sm.h
@@ -37,7 +37,7 @@
 
 /*==========================================================================*/
 /*!
- * @addtogroup BRD_SM_MX95EVK
+ * @addtogroup BRD_SM_MX95LIBRA
  * @{
  *
  * @file
@@ -51,17 +51,15 @@
 
 #include "sm.h"
 #include "brd_sm_handlers.h"
-#include "brd_sm_control.h"
 #include "brd_sm_sensor.h"
 #include "brd_sm_voltage.h"
-#include "brd_sm_bbm.h"
 #include "board.h"
 #include "brd_sm_api.h"
 
 /* Defines */
 
 /*! Board name string */
-#define BRD_SM_NAME  "i.MX95 EVK"
+#define BRD_SM_NAME  "Libra i.MX95"
 
 /*! Board attributes */
 #define BRD_SM_ATTR  0x0

commit 60b873c20a74c73326a581a57d72ee9454ba520b

board: mcimx95libra: enable the BBNSM irq
We are interested in the ONOFF button state.

This was accidentally removed with the RTC removal
Fixes: de05d4a91455 ("Adapt copied code to Libra i.MX 95")

Signed-off-by: Jan Remmet <j.remmet@phytec.de>

boards/mcimx95libra/board.c
@@ -304,6 +304,9 @@ void BOARD_InitHandlers(void) {
     /* Configure SWI handler */
     NVIC_EnableIRQ(BOARD_SWI_IRQn);
 
+    /* Enable BBNSM handler */
+    NVIC_EnableIRQ(BBNSM_IRQn);
+
     /* Enable GPC SM handler */
     NVIC_SetPriority(GPC_SM_REQ_IRQn, IRQ_PRIO_NOPREEMPT_VERY_HIGH);
     NVIC_EnableIRQ(GPC_SM_REQ_IRQn);